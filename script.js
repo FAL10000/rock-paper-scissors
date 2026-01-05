39a711d54b48a02dbf158d35fc7a3d537fee76b2
Naufal <mnaufal3@gatech.edu>
2026-01-05 09:03:26 +0700

    Added computer & human choice mechanism
    Computer can now return 3 choices randomly.
    Human choice is now a button with a prompt.
    After selecting a choice, a log will be printed on the screen.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 1) {
+        return 'Rock';
+    } else if (randomNumber === 2) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
+function getHumanChoice() {
+    return prompt('What is your choice?');
+}
+
+const signButton = document.querySelector("#signButton");
+const log = document.querySelector("#log");
+
+signButton.addEventListener("click", () => {
+    let humanChoice = getHumanChoice();
+    log.textContent += getComputerChoice() + " VS " + humanChoice;
+});
